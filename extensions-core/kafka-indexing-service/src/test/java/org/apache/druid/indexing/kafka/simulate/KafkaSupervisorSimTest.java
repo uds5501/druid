cd9a94810d07e011b7fd36a800648b574b22f430
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2025-06-24 22:08:04 +0530

    Add autoscaler kafka test

--- extensions-core/kafka-indexing-service/src/test/java/org/apache/druid/indexing/kafka/simulate/KafkaSupervisorSimTest.java
+++ extensions-core/kafka-indexing-service/src/test/java/org/apache/druid/indexing/kafka/simulate/KafkaSupervisorSimTest.java
@@ -31,6 +31,8 @@ import org.apache.druid.indexing.kafka.supervisor.KafkaSupervisorIOConfig;
 import org.apache.druid.indexing.kafka.supervisor.KafkaSupervisorSpec;
 import org.apache.druid.indexing.kafka.supervisor.KafkaSupervisorTuningConfig;
 import org.apache.druid.indexing.overlord.supervisor.SupervisorStatus;
+import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.AutoScalerConfig;
+import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.LagBasedAutoScalerConfig;
 import org.apache.druid.java.util.common.DateTimes;
 import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.query.DruidMetrics;
@@ -38,11 +40,15 @@ import org.apache.druid.segment.indexing.DataSchema;
 import org.apache.druid.testing.simulate.EmbeddedBroker;
 import org.apache.druid.testing.simulate.EmbeddedCoordinator;
 import org.apache.druid.testing.simulate.EmbeddedDruidCluster;
+import org.apache.druid.testing.simulate.EmbeddedHistorical;
 import org.apache.druid.testing.simulate.EmbeddedIndexer;
 import org.apache.druid.testing.simulate.EmbeddedOverlord;
+import org.apache.druid.testing.simulate.EmbeddedRouter;
+import org.apache.druid.testing.simulate.emitter.LatchableEmitterModule;
 import org.apache.druid.testing.simulate.junit5.IndexingSimulationTestBase;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.joda.time.DateTime;
+import org.joda.time.Period;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -56,6 +62,7 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
   private final EmbeddedBroker broker = new EmbeddedBroker();
   private final EmbeddedIndexer indexer = new EmbeddedIndexer();
   private final EmbeddedOverlord overlord = new EmbeddedOverlord();
+  private final EmbeddedHistorical historical = new EmbeddedHistorical();
   private EmbeddedKafkaServer kafkaServer;
 
   @Override
@@ -64,14 +71,18 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
     final EmbeddedDruidCluster cluster = EmbeddedDruidCluster.withEmbeddedDerbyAndZookeeper();
 
     kafkaServer = new EmbeddedKafkaServer(cluster.getZookeeper(), cluster.getTestFolder(), Map.of());
+    indexer.addProperty("druid.worker.capacity", "10");
 
     cluster.addExtension(KafkaIndexTaskModule.class)
+           .addExtension(LatchableEmitterModule.class)
            .addResource(kafkaServer)
            .useLatchableEmitter()
            .addServer(new EmbeddedCoordinator())
            .addServer(overlord)
            .addServer(indexer)
-           .addServer(broker);
+           .addServer(broker)
+           .addServer(historical)
+           .addServer(new EmbeddedRouter());
 
     return cluster;
   }
@@ -125,7 +136,102 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
     Assertions.assertTrue(supervisorStatus.isSuspended());
   }
 
+  @Test
+  public void test_runKafkaSupervisor_withAutoScaler()
+  {
+    final String topic = dataSource;
+    final int partitionCount = 15;
+    final int recordsPerPartition = 5;
+    final int taskCountMin = 3;
+
+    kafkaServer.createTopicWithPartitions(topic, partitionCount);
+
+    kafkaServer.produceRecordsToTopic(
+        generateRecordsForTopicPerPartition(topic, recordsPerPartition, DateTimes.of("2025-06-01"), partitionCount)
+    );
+
+    // Submit and start a supervisor
+    final String supervisorId = dataSource + "_supe";
+    final AutoScalerConfig autoScalerConfig = new LagBasedAutoScalerConfig(
+        null, null, null, null, null, null, null, null,
+        partitionCount, null, taskCountMin, null, null, true, null, null
+    );
+    final KafkaSupervisorIOConfig supervisorIOConfig = new KafkaSupervisorIOConfig(
+        topic,
+        null,
+        new CsvInputFormat(List.of("timestamp", "item"), null, null, false, 0, false),
+        null, null,
+        new Period("PT1M"),
+        kafkaServer.consumerProperties(),
+        autoScalerConfig, null, null, null, null,
+        true,
+        new Period("PT2M"), null, null, null, null, null, null, null
+    );
+    final KafkaSupervisorSpec kafkaSupervisorSpec = createKafkaSupervisor(supervisorId, topic, supervisorIOConfig);
+
+    final Map<String, String> startSupervisorResult = getResult(
+        cluster.leaderOverlord().postSupervisor(kafkaSupervisorSpec)
+    );
+    Assertions.assertEquals(Map.of("id", supervisorId), startSupervisorResult);
+
+    // Wait for the broker to discover the realtime segments
+    broker.latchableEmitter().waitForEvent(
+        event -> event.hasDimension(DruidMetrics.DATASOURCE, dataSource)
+    );
+
+    SupervisorStatus supervisorStatus = getSupervisorStatus(supervisorId);
+    Assertions.assertFalse(supervisorStatus.isSuspended());
+    Assertions.assertTrue(supervisorStatus.isHealthy());
+    Assertions.assertEquals(dataSource, supervisorStatus.getDataSource());
+    Assertions.assertEquals("RUNNING", supervisorStatus.getState());
+    Assertions.assertEquals(topic, supervisorStatus.getSource());
+
+
+    // Get the task ID
+    List<TaskStatusPlus> taskStatuses = ImmutableList.copyOf(
+        getResult(cluster.leaderOverlord().taskStatuses(null, dataSource, partitionCount))
+    );
+    Assertions.assertEquals(taskCountMin, taskStatuses.size());
+    Assertions.assertEquals(TaskState.RUNNING, taskStatuses.get(0).getStatusCode());
+
+    overlord.latchableEmitter().waitForEventAggregate(
+        event -> event.hasMetricName("task/run/time")
+                      .hasDimension(DruidMetrics.DATASOURCE, dataSource),
+        agg -> agg.hasCount(taskCountMin)
+    );
+
+    // Verify the count of rows ingested into the datasource so far
+    final int numRows = Integer.parseInt(
+        runSql("SELECT COUNT(*) FROM %s", dataSource)
+    );
+    Assertions.assertEquals(partitionCount * recordsPerPartition, numRows);
+
+    // Suspend the supervisor and verify the state
+    getResult(
+        cluster.leaderOverlord().postSupervisor(kafkaSupervisorSpec.createSuspendedSpec())
+    );
+    supervisorStatus = getSupervisorStatus(supervisorId);
+    Assertions.assertTrue(supervisorStatus.isSuspended());
+  }
+
   private KafkaSupervisorSpec createKafkaSupervisor(String supervisorId, String topic)
+  {
+    return createKafkaSupervisor(
+        supervisorId, topic, new KafkaSupervisorIOConfig(
+            topic,
+            null,
+            new CsvInputFormat(List.of("timestamp", "item"), null, null, false, 0, false),
+            null, null,
+            null,
+            kafkaServer.consumerProperties(),
+            null, null, null, null, null,
+            true,
+            null, null, null, null, null, null, null, null
+        )
+    );
+  }
+
+  private KafkaSupervisorSpec createKafkaSupervisor(String supervisorId, String topic, KafkaSupervisorIOConfig ioConfig)
   {
     return new KafkaSupervisorSpec(
         supervisorId,
@@ -136,17 +242,7 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
                   .withDimensions(DimensionsSpec.EMPTY)
                   .build(),
         createTuningConfig(),
-        new KafkaSupervisorIOConfig(
-            topic,
-            null,
-            new CsvInputFormat(List.of("timestamp", "item"), null, null, false, 0, false),
-            null, null,
-            null,
-            kafkaServer.consumerProperties(),
-            null, null, null, null, null,
-            true,
-            null, null, null, null, null, null, null, null
-        ),
+        ioConfig,
         null, null, null, null, null, null, null, null, null, null, null
     );
   }
@@ -162,10 +258,34 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
     );
   }
 
+  private List<ProducerRecord<byte[], byte[]>> generateRecordsForTopicPerPartition(
+      String topic,
+      int count,
+      DateTime startTime,
+      int partitionCount
+  )
+  {
+    List<ProducerRecord<byte[], byte[]>> records = new ArrayList<>();
+    for (int partition = 0; partition < partitionCount; partition++) {
+      records.addAll(generateRecordsForTopic(topic, count, startTime, partition));
+    }
+    return records;
+  }
+
   private List<ProducerRecord<byte[], byte[]>> generateRecordsForTopic(
       String topic,
       int count,
       DateTime startTime
+  )
+  {
+    return generateRecordsForTopic(topic, count, startTime, 0);
+  }
+
+  private List<ProducerRecord<byte[], byte[]>> generateRecordsForTopic(
+      String topic,
+      int count,
+      DateTime startTime,
+      int partition
   )
   {
     final List<ProducerRecord<byte[], byte[]>> records = new ArrayList<>();
@@ -177,7 +297,7 @@ public class KafkaSupervisorSimTest extends IndexingSimulationTestBase
           ThreadLocalRandom.current().nextInt(1000)
       );
       records.add(
-          new ProducerRecord<>(topic, 0, null, StringUtils.toUtf8(valueCsv))
+          new ProducerRecord<>(topic, partition, null, StringUtils.toUtf8(valueCsv))
       );
     }
     return records;
